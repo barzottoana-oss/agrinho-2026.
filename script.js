21de16be6967e2e3db285f13230673bb3ccde0ce
barzottoana-oss <barzotto.ana@escola.pr.gov.br>
2026-05-28 08:36:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Aguarda o DOM carregar completamente
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("btn-interativo");
+
+    botao.addEventListener("click", () => {
+        alert("Obrigado por se interessar pelo Projeto Agrinho! Juntos transformamos a educação e o campo.");
+    });
+});
